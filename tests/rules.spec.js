f2bd525d8fd06a2ed0e1425280f763fcb28bd152
timwis <tim@timwis.com>
2018-06-12 17:58:10 -0400

    Update rules to new format, and fix rules tests

--- tests/rules.spec.js
+++ tests/rules.spec.js
@@ -7,20 +7,43 @@ const rules = require('../rules.json')
 
 describe('rule validations', () => {
   for (const rule of rules) {
-    describe(rule.pattern, () => {
-      test('valid type', () => {
-        expect(rule.type).toMatch(/redirect|rewrite/)
-      })
-
-      if (rule.regex) {
+    const ruleName = rule.test.path_exact || rule.test.path_pattern
+    describe(ruleName, () => {
+      if (rule.test.path_pattern) {
         test('valid regex', () => {
-          expect(() => new RegExp(rule.pattern)).not.toThrow()
+          expect(() => new RegExp(rule.test.path_pattern)).not.toThrow()
         })
-      } else {
-        test('no regex matches in non-regex rule', () => {
-          expect(rule.pattern).not.toContain('(')
-          expect(rule.pattern).not.toContain('$')
-          expect(rule.pattern).not.toContain('^')
+
+        test(`a test can't have both path_pattern and path_exact`, () => {
+          expect(rule.test).not.toHaveProperty('path_exact')
+        })
+      } else if (rule.test.path_exact) {
+        test('no regex matches in path_exact', () => {
+          expect(rule.test.path_exact).not.toContain('(')
+          expect(rule.test.path_exact).not.toContain('$')
+          expect(rule.test.path_exact).not.toContain('^')
+        })
+      }
+
+      if (rule.redirect) {
+        test(`a redirect rule can't also have a rewrite rule`, () => {
+          expect(rule).not.toHaveProperty('rewrite')
+        })
+
+        test('has a location property', () => {
+          expect(rule.redirect).toHaveProperty('location')
+        })
+
+        test('does not have an origin property', () => {
+          expect(rule.redirect).not.toHaveProperty('origin')
+        })
+      } else if (rule.rewrite) {
+        test('has a path property', () => {
+          expect(rule.rewrite).toHaveProperty('path')
+        })
+
+        test('does not have a location property', () => {
+          expect(rule.rewrite).not.toHaveProperty('location')
         })
       }
     })
